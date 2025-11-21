62cf2f3eb00cd9e74123b97cfd8c57885b1a28be
sohammitra777 <soham.business777@gamil.com>
2025-11-21 11:55:31 +0530

    general and qol improvement

--- public/index.js
+++ public/index.js
@@ -1,56 +1,61 @@
-const url = "http://localhost:3000" 
+const url = "https://blogpost-dori.onrender.com"
+
+
 
 setInterval(() => {
     window.location.reload();
-    alert("5 minute passed"); 
-}, 5*60*1000);
+    alert("5 minute passed");
+}, 5 * 60 * 1000);
+
+const form = document.getElementById("form");
+form.setAttribute("action", url + "/submit");
+
 //Patch Function
-function patchFunction(user_id, titleText, bodyText){
-    return fetch(url + '/users/' + user_id, {
-        method : 'PATCH', 
-        headers : {
-            'Content-Type' : 'application/json'
+function patchFunction(user_id, titleText, bodyText) {
+    return fetch(url + "/users/" + user_id, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
         },
-        body : JSON.stringify({
-            title : titleText, 
-            body : bodyText
+        body: JSON.stringify({
+            title: titleText,
+            body: bodyText,
+        }),
+    })
+        .then((res) => {
+            if (!res.ok) throw Error();
+            return res.json();
         })
-    })
-    .then(res => {
-        if(!res.ok) throw Error(); 
-        return res.json(); 
-    })
-    .catch(err => console.log(err)); 
+        .catch((err) => console.log(err));
 }
 
 //Put Function
-function putFunction(user_id, titleText, bodyText){
-    return fetch(url + '/users/' + user_id, {
-        method : 'PUT', 
-        headers : {
-            'Content-Type' : 'application/json'
+function putFunction(user_id, titleText, bodyText) {
+    return fetch(url + "/users/" + user_id, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
         },
-        body : JSON.stringify({
-            title : titleText, 
-            body : bodyText
+        body: JSON.stringify({
+            title: titleText,
+            body: bodyText,
+        }),
+    })
+        .then((res) => {
+            if (!res.ok) throw Error();
+            return res.json();
         })
-    })
-    .then(res => {
-        if(!res.ok) throw Error(); 
-        return res.json(); 
-    })
-    .catch(err => console.log(err)); 
+        .catch((err) => console.log(err));
 }
 
 //Edit Function
-function editFunction(user_id){
-
+function editFunction(user_id) {
     //selecting elements
-    const parentDiv = document.getElementById(user_id); 
-    const titleChild = parentDiv.querySelector('h2');  
-    const bodyChild = parentDiv.querySelector('p');
-    const editChild = parentDiv.querySelector('.editable');
-    editChild.remove(); 
+    const parentDiv = document.getElementById(user_id);
+    const titleChild = parentDiv.querySelector("h2");
+    const bodyChild = parentDiv.querySelector("p");
+    const editChild = parentDiv.querySelector(".editable");
+    editChild.remove();
 
     //saving inner text
     const savedTitle = titleChild.innerText;
@@ -58,130 +63,129 @@ function editFunction(user_id){
 
     //making content editable and changing apperence
     //making editable
-    titleChild.setAttribute('contenteditable', 'true'); 
-    bodyChild.setAttribute('contenteditable', 'true'); 
-    
+    titleChild.setAttribute("contenteditable", "true");
+    bodyChild.setAttribute("contenteditable", "true");
+
     //changing appearence
-    titleChild.setAttribute('class', 'change'); 
-    bodyChild.setAttribute('class', 'change');
-    
+    titleChild.setAttribute("class", "change");
+    bodyChild.setAttribute("class", "change");
+
     //Creating element
-    const saveButton = document.createElement('button'); 
-    const cancelButton = document.createElement('button'); 
+    const saveButton = document.createElement("button");
+    const cancelButton = document.createElement("button");
 
     //Setting the text of the element
-    saveButton.innerText = 'Save'; 
-    cancelButton.innerText = 'Cancel'; 
-
+    saveButton.innerText = "Save";
+    cancelButton.innerText = "Cancel";
 
     //Adding Event Listners
-    cancelButton.addEventListener('click', ()=>{
-        titleChild.removeAttribute('contenteditable'); 
-        bodyChild.removeAttribute('contenteditable');
+    cancelButton.addEventListener("click", () => {
+        titleChild.removeAttribute("contenteditable");
+        bodyChild.removeAttribute("contenteditable");
 
-        titleChild.removeAttribute('class'); 
-        bodyChild.removeAttribute('class');
+        titleChild.removeAttribute("class");
+        bodyChild.removeAttribute("class");
 
-        cancelButton.remove(); 
-        saveButton.remove(); 
+        cancelButton.remove();
+        saveButton.remove();
 
-        titleChild.innerText = savedTitle; 
-        bodyChild.innerText = savedBody; 
-        parentDiv.append(editChild); 
-    })
-    
+        titleChild.innerText = savedTitle;
+        bodyChild.innerText = savedBody;
+        parentDiv.append(editChild);
+    });
 
-    saveButton.addEventListener('click', ()=>{
-        const titleInnerText = titleChild.innerText; 
-        const bodyInnerText = bodyChild.innerText; 
+    saveButton.addEventListener("click", () => {
+        const titleInnerText = titleChild.innerText;
+        const bodyInnerText = bodyChild.innerText;
 
+        if (savedBody !== bodyInnerText && titleInnerText !== savedTitle)
+            putFunction(user_id, titleInnerText, bodyInnerText);
+        else if (savedBody === bodyInnerText && titleInnerText === savedTitle) {
+        } else patchFunction(user_id, titleInnerText, bodyInnerText);
 
-        if(savedBody !== bodyInnerText && titleInnerText !==savedTitle ) putFunction(user_id, titleInnerText, bodyInnerText); 
-        else if(savedBody === bodyInnerText && titleInnerText === savedTitle){}
-        else patchFunction(user_id, titleInnerText, bodyInnerText); 
+        titleChild.removeAttribute("contenteditable");
+        bodyChild.removeAttribute("contenteditable");
 
-        titleChild.removeAttribute('contenteditable'); 
-        bodyChild.removeAttribute('contenteditable'); 
-        
-        
-        titleChild.removeAttribute('class'); 
-        bodyChild.removeAttribute('class');
+        titleChild.removeAttribute("class");
+        bodyChild.removeAttribute("class");
 
-        cancelButton.remove(); 
-        saveButton.remove(); 
+        cancelButton.remove();
+        saveButton.remove();
 
         parentDiv.append(editChild);
-    })
+    });
 
     //Appending elements
-    parentDiv.append(cancelButton); 
-    parentDiv.append(saveButton);  
+    parentDiv.append(cancelButton);
+    parentDiv.append(saveButton);
 }
 
 //Delete Function
-function deleteFunction(user_id){
-    return fetch(url + '/users/' + user_id, {
-        method : 'DELETE'
+function deleteFunction(user_id) {
+    return fetch(url + "/users/" + user_id, {
+        method: "DELETE",
     })
-    .then(res => {
-        if(!res.ok) throw Error(); 
-        return;  
-    })
-    .then(()=>{
-        const id_name = document.getElementById(user_id); 
-        id_name.remove(); 
-    })
-    .catch((err) => console.log(err)); 
+        .then((res) => {
+            if (!res.ok) throw Error();
+            return;
+        })
+        .then(() => {
+            const id_name = document.getElementById(user_id);
+            id_name.remove();
+        })
+        .catch((err) => console.log(err));
 }
 
 //Get Function
-function getFunction(){
-    return fetch(url + '/users')
-    .then(res => {
-        if(!res.ok) throw Error(); 
-        return res.json(); 
-    }) 
-    .then(data => {
-        if(data.valueExist === false) return; 
+function getFunction() {
+    return fetch(url + "/users")
+        .then((res) => {
+            if (!res.ok) throw Error();
+            return res.json();
+        })
+        .then((data) => {
+            if (data.valueExist === false) return;
 
-        const users = data.userList
+            const users = data.userList;
 
-        //Aquiring main div to attach
-        const mainDiv = document.getElementById('post-here'); 
+            //Aquiring main div to attach
+            const mainDiv = document.getElementById("post-here");
 
-        for(let key in users){
-            //creating required elements
-            const innerDiv = document.createElement('div'); 
-            const titleElement = document.createElement('h2'); 
-            const bodyElement = document.createElement('p'); 
-            const editButton = document.createElement('button'); 
-            const deleteButton = document.createElement('button'); 
+            for (let key in users) {
+                //creating required elements
+                const innerDiv = document.createElement("div");
+                const titleElement = document.createElement("h2");
+                const bodyElement = document.createElement("p");
+                const editButton = document.createElement("button");
+                const deleteButton = document.createElement("button");
 
-            //setting the attribute for the required element
-            innerDiv.setAttribute('class', 'hosting-div');
-            innerDiv.setAttribute('id', key);
-            editButton.setAttribute('class', 'editable');
-            deleteButton.setAttribute('class', 'deletable'); 
-            
-            //Setting innerHTML
-            titleElement.innerHTML = users[key].title;
-            bodyElement.innerHTML = users[key].body;
-            editButton.innerHTML = 'Edit'; 
-            deleteButton.innerHTML = 'Delete'; 
+                //setting the attribute for the required element
+                innerDiv.setAttribute("class", "hosting-div");
+                innerDiv.setAttribute("id", key);
+                editButton.setAttribute("class", "editable");
+                deleteButton.setAttribute("class", "deletable");
 
-            //Appending elements
-            mainDiv.append(innerDiv);
-            innerDiv.append(titleElement); 
-            innerDiv.append(bodyElement);
-            innerDiv.append(deleteButton); 
-            innerDiv.append(editButton); 
+                //Setting innerHTML
+                titleElement.innerHTML = users[key].title;
+                bodyElement.innerHTML = users[key].body;
+                editButton.innerHTML = "Edit";
+                deleteButton.innerHTML = "Delete";
 
-            //EventListners for custom functionality
-            editButton.addEventListener('click', ()=> editFunction(key)); 
-            deleteButton.addEventListener('click', ()=> deleteFunction(key)); 
-        }  
-    })
-    .catch(err => console.error(err)); 
+                //Appending elements
+                mainDiv.append(innerDiv);
+                innerDiv.append(titleElement);
+                innerDiv.append(bodyElement);
+                innerDiv.append(deleteButton);
+                innerDiv.append(editButton);
+
+                //EventListners for custom functionality
+                editButton.addEventListener("click", () => editFunction(key));
+                deleteButton.addEventListener("click", () =>
+                    deleteFunction(key)
+                );
+            }
+        })
+        .catch((err) => console.error(err));
 }
 
-getFunction(); 
+getFunction();
